792b1d26db7f6dab4a56bb68d68bca5ddc1e06cf
Lucassmith <lucasbsmith19@gmail.com>
2024-05-18 14:26:30 -0500

    Initialize repository

---
+++ lib/lbsmith_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lbsmith_palindrome/version"
+
+module LbsmithPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lbsmith_palindrome_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LbsmithPalindrome do
+  it "has a version number" do
+    expect(LbsmithPalindrome::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
